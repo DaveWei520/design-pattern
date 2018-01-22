970d57943d27f92d0ac939b62636a034a03fc5d2
weiyu <weiyu@gomeholdings.com>
2018-01-22 14:19:55 +0800

    修改策略模式调用示例代码

--- responsibility-chain/src/main/java/com/weiyu/responsibilityChain/demo/ResponsibilityChainClient.java
+++ responsibility-chain/src/main/java/com/weiyu/responsibilityChain/demo/ResponsibilityChainClient.java
@@ -5,6 +5,7 @@ package com.weiyu.responsibilityChain.demo;
  * 使多个对象都有机会处理请求，从而避免了请求的发送者和接收者之间的耦合关系。
  * 将这些对象连成一条链，并沿着这条链传递该请求，直到有对象处理它为止
  * @description 责任链模式
+ * @type 行为类模式
  * @create 2018/01/22
  */
 class Level {
