ef39246bd2592d0c6825bc8b25f53d8f38bbd333
weiyu <weiyu@gomeholdings.com>
2018-04-28 15:07:07 +0800

    更新构造者模式示例

---
+++ design-pattern-core/src/main/java/com/weiyu/designPattern/builder/ConcreteBuilder.java
@@ -0,0 +1,16 @@
+package com.weiyu.designPattern.builder;
+
+public class ConcreteBuilder extends Builder{
+    private Product product = new Product();
+
+    @Override
+    public void setPart(String arg1, String arg2) {
+        product.setName(arg1);
+        product.setType(arg2);
+    }
+
+    @Override
+    public Product getProduct() {
+        return product;
+    }
+}
