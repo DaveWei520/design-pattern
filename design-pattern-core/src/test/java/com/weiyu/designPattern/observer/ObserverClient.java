d455ed33e03e686ab6a9756209c3abbd867a4330
weiyu <weiyu@gomeholdings.com>
2018-05-14 16:28:00 +0800

    修改观察者模式

--- design-pattern-core/src/test/java/com/weiyu/designPattern/observer/ObserverClient.java
+++ design-pattern-core/src/test/java/com/weiyu/designPattern/observer/ObserverClient.java
@@ -13,10 +13,19 @@ public class ObserverClient {
     @Test
     public void test(){
         WechatServer server = new WechatServer();
-        server.registerObserver(new User("张三"));
-        server.registerObserver(new User("李四"));
-        server.registerObserver(new User("王五"));
+        Observer userZhang = new User("张三");
+        Observer userLi = new User("李四");
+        Observer userWang = new User("王五");
+
+        server.registerObserver(userZhang);
+        server.registerObserver(userLi);
+        server.registerObserver(userWang);
 
         server.setInfomation("服务器即将维护更新");
+
+        System.out.println("======移除张三");
+        //移除张三
+        server.removeObserver(userZhang);
+        server.setInfomation("再次发布更新消息");
     }
 }
