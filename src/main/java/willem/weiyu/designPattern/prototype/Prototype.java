f751c4230b3d08f3347edd594f5d8090114d42b5
weiyu <weiyu@gomefinance.com.cn>
2018-08-20 10:39:12 +0800

    添加深浅拷贝

--- src/main/java/willem/weiyu/designPattern/prototype/Prototype.java
+++ src/main/java/willem/weiyu/designPattern/prototype/Prototype.java
@@ -1,5 +1,8 @@
 package willem.weiyu.designPattern.prototype;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author weiyu
  * 用原型实例指定创建对象的种类，并通过拷贝这些原型创建新的对象
@@ -9,6 +12,24 @@ package willem.weiyu.designPattern.prototype;
 class Prototype implements Cloneable {
     private String name;
     private String type;
+    private ArrayList<String> elemList = new ArrayList<>();
+    private Person person = new Person();
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public List<String> getElemList() {
+        return elemList;
+    }
+
+    public void setElemList(String elem) {
+        this.elemList.add(elem);
+    }
 
     public String getName() {
         return name;
@@ -29,10 +50,41 @@ class Prototype implements Cloneable {
     public Prototype clone(){
         Prototype prototype = null;
         try{
+            //浅拷贝
             prototype = (Prototype) super.clone();
+            //深拷贝
+            //prototype.elemList = (ArrayList<String>) this.elemList.clone();
+            //prototype.person = (Person) this.person.clone();
         }catch (CloneNotSupportedException e){
             e.printStackTrace();
         }
         return prototype;
+    }
+}
+
+class Person implements Cloneable{
+    private String name;
+    //0男，1女
+    private int sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 }
--- src/test/java/willem/weiyu/designPattern/prototype/PrototypeClient.java
+++ src/test/java/willem/weiyu/designPattern/prototype/PrototypeClient.java
@@ -8,9 +8,18 @@ public class PrototypeClient {
     public void test() {
         Prototype object = new Prototype();
         object.setName("clone");
-        for (int i = 0; i < 10; i++){
-            Prototype clone = object.clone();
-            System.out.println(clone+"======name:"+clone.getName());
-        }
+        object.setElemList("first");
+        object.getPerson().setName("zhangsan");
+        System.out.println(object + "原对象======name：" + object.getName());
+        System.out.println(object+ "原对象======element：" + object.getElemList());
+        System.out.println(object+ "原对象======person：" + object.getPerson().getName());
+
+        Prototype clone = object.clone();
+        clone.setName("newClone");
+        clone.setElemList("second");
+        clone.getPerson().setName("lisi");
+        System.out.println(clone + "拷贝对象======name：" + clone.getName());
+        System.out.println(clone + "拷贝对象======element：" + clone.getElemList());
+        System.out.println(clone + "拷贝对象======element：" + clone.getPerson().getName());
     }
 }
